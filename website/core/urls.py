34454d31f568adce1f9e9454347b52ee3a07016a
Anna Makarudze <amakarudze@gmail.com>
2026-01-10 01:19:35 +0100

    Change blog path on website to fix url conflicts

--- website/core/urls.py
+++ website/core/urls.py
@@ -6,7 +6,7 @@ from . import views
 urlpatterns = [
     path("", views.HomePageView.as_view(), name="home"),
     path("about/", views.AboutPageView.as_view(), name="about"),
-    path("blog/", views.BlogPageView.as_view(), name="blog"),
-    path("blog/post_detail/<slug:slug>/", views.BlogDetailView.as_view(), name="post_detail"),
+    path("posts/", views.BlogPageView.as_view(), name="posts"),
+    path("posts/post_detail/<slug:slug>/", views.BlogDetailView.as_view(), name="post_detail"),
     path("contact/", views.ContactPageView.as_view(), name="contact"),
 ]
