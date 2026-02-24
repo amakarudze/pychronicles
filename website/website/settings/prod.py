5704d43249b5296b35c580d688dfff3f33e70989
Anna Makarudze <amakarudze@gmail.com>
2026-02-24 01:45:53 +0100

    Add logging settings

--- website/website/settings/prod.py
+++ website/website/settings/prod.py
@@ -66,7 +66,7 @@ LOGGING = {
     "loggers": {
         "django": {
             "handlers": ["console"],
-            "level": os.getenv("DJANGO_LOG_LEVEL", "INFO"),
+            "level": os.getenv("DJANGO_LOG_LEVEL", "INFO"), # noqa: F405
             "propagate": False,
         },
     },
