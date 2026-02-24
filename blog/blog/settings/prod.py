a972318122512941afb10d1ebe2d6df53675857f
Anna Makarudze <amakarudze@gmail.com>
2026-02-24 01:41:05 +0100

    Add logging settings

--- blog/blog/settings/prod.py
+++ blog/blog/settings/prod.py
@@ -48,3 +48,24 @@ CSRF_TRUSTED_ORIGINS = [
 ]
 
 SECURE_REFERRER_POLICY = "same-origin"
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "WARNING",
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["console"],
+            "level": os.getenv("DJANGO_LOG_LEVEL", "INFO"),
+            "propagate": False,
+        },
+    },
+}
