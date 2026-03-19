ef49d2e12bbbf73c7a774086475ee53ac37dc3d8
Anna Makarudze <amakarudze@gmail.com>
2026-03-19 21:23:01 +0100

    Fix blog media url path

--- website/website/settings/prod.py
+++ website/website/settings/prod.py
@@ -50,7 +50,7 @@ CSRF_TRUSTED_ORIGINS = [
 SECURE_REFERRER_POLICY = "same-origin"
 
 BLOG_API = "https://pychronicles.com/blog/api/blog"
-BLOG_MEDIA_URL = "https://pychronicles.com/blog"
+BLOG_MEDIA_URL = "https://pychronicles.com"
 
 LOGGING = {
     "version": 1,
